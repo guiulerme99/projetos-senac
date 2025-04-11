b8238eb97e8e97ef6afb1cacf2b963984904aaec
guiulerme99 <guihrsilva99@gmail.com>
2025-04-11 19:42:46 -0300

    iniciando variaveis no JS

---
+++ Proj_JurosCompostos/site/script.js
@@ -0,0 +1,6 @@
+let valorInicial = document.getElementById("campo-valor").value;
+let investimentoMensal = document.getElementById("campo-investimento").value;
+let juros = document.getElementById("campo-juros").value;
+let qntdMeses = document.getElementById("campo-meses").value;
+let mesInicial = document.getElementById("campo-mes-inicial").value;
+let anoInicial = document.getElementById("campo-ano-inicial").value;
