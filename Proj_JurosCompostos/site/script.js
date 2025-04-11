2f323cb7fafe9d6b48021ddcfd94e1fa42d76d50
guiulerme99 <guihrsilva99@gmail.com>
2025-04-11 20:06:34 -0300

    Adiciando calculo de juros ao ano para juros ao mes

--- Proj_JurosCompostos/site/script.js
+++ Proj_JurosCompostos/site/script.js
@@ -1,6 +1,15 @@
-let valorInicial = document.getElementById("campo-valor").value;
-let investimentoMensal = document.getElementById("campo-investimento").value;
-let juros = document.getElementById("campo-juros").value;
-let qntdMeses = document.getElementById("campo-meses").value;
-let mesInicial = document.getElementById("campo-mes-inicial").value;
-let anoInicial = document.getElementById("campo-ano-inicial").value;
+function calcJuros() {
+
+    let valorInicial = document.getElementById("campo-valor").value;
+    let investimentoMensal = document.getElementById("campo-investimento").value;
+    let juros = document.getElementById("campo-juros").value;
+    let qntdMeses = document.getElementById("campo-meses").value;
+    let mesInicial = document.getElementById("campo-mes-inicial").value;
+    let anoInicial = document.getElementById("campo-ano-inicial").value;
+
+    let taxaMensal = Math.pow(1+juros/100, 1/12) - 1;
+    document.getElementById("campo1").innerHTML = taxaMensal;
+
+
+
+}
