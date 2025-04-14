b6ac1a3b6db1c798e2f8725b5af03939bb7c6b64
guiulerme99 <guihrsilva99@gmail.com>
2025-04-14 20:28:46 -0300

    atividade completa

--- Proj_JurosCompostos/site/script.js
+++ Proj_JurosCompostos/site/script.js
@@ -4,14 +4,21 @@ function calcJuros() {
     let investimentoMensal = document.getElementById("campo-investimento").value;
     let juros = document.getElementById("campo-juros").value;
     let qntdMeses = document.getElementById("campo-meses").value;
-    let mesInicial = document.getElementById("campo-mes-inicial").value;
+    let mesInicial = Math.round(document.getElementById("campo-mes-inicial").value);
     let anoInicial = document.getElementById("campo-ano-inicial").value;
 
-    
-
     let taxaMensal = Math.pow(1+juros/100, 1/12) - 1;
-    document.getElementById("campo1").innerHTML = taxaMensal;
+    // document.getElementById("test").innerHTML = taxaMensal;
 
-
-
+    let meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+    let i = 0;
+    for (i; i < qntdMeses; i++) {
+        document.getElementById("tabela").innerHTML += 
+            `<tr>
+                <td> ` + (i+1) + `</td> 
+                <td> ` + meses[(mesInicial + i) % 12] + ` </td>
+                <td> ` + anoInicial + ` </td>
+             </tr>`;
+    }
 }
+
