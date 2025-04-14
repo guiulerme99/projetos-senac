44caf566a281a882ff34e35891d1d617bbd0f1b5
guiulerme99 <guihrsilva99@gmail.com>
2025-04-14 20:29:18 -0300

    atividade completa

--- Proj_JurosCompostos/site/script.js
+++ Proj_JurosCompostos/site/script.js
@@ -1,24 +1,39 @@
 function calcJuros() {
 
-    let valorInicial = document.getElementById("campo-valor").value;
-    let investimentoMensal = document.getElementById("campo-investimento").value;
+    let valorInicial = Number(document.getElementById("campo-valor").value);
+    let investimentoMensal = Number(document.getElementById("campo-investimento").value);
     let juros = document.getElementById("campo-juros").value;
     let qntdMeses = document.getElementById("campo-meses").value;
-    let mesInicial = Math.round(document.getElementById("campo-mes-inicial").value);
-    let anoInicial = document.getElementById("campo-ano-inicial").value;
+    let mesInicial = Number(Math.round(document.getElementById("campo-mes-inicial").value));
+    let anoInicial = Number(document.getElementById("campo-ano-inicial").value);
 
     let taxaMensal = Math.pow(1+juros/100, 1/12) - 1;
     // document.getElementById("test").innerHTML = taxaMensal;
 
     let meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
-    let i = 0;
+    let i = 0
+    let rendimento = 0, valorFinal = 0, contMes = mesInicial;
+    juros = (juros/12) / 100;
     for (i; i < qntdMeses; i++) {
+        
+        if (contMes == 12) {
+            anoInicial++;
+            contMes = 0;
+        }
+        rendimento = Number(valorInicial * juros);
+        valorFinal = valorInicial+rendimento;
         document.getElementById("tabela").innerHTML += 
             `<tr>
                 <td> ` + (i+1) + `</td> 
                 <td> ` + meses[(mesInicial + i) % 12] + ` </td>
                 <td> ` + anoInicial + ` </td>
+                <td> ` + valorInicial + `</td>
+                <td> ` + rendimento + `</td>
+                <td> ` + valorFinal + `</td>
              </tr>`;
+        valorInicial += investimentoMensal+rendimento;
+        rendimento = 0;
+        contMes++;
     }
 }
 
